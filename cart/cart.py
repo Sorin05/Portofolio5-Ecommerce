6e3f4b16bee4a4ee1b3bc8c7e7bd872dcb20914b
Sorin05 <sorinandrei230587@gmail.com>
2022-08-16 15:58:40 +0000

    Create session and context processors

---
+++ cart/cart.py
@@ -0,0 +1,16 @@
+
+
+
+class Cart():
+    """
+    A shopping cart class, with some defalt behaviors that
+    can  be inherited or overrided.
+    """
+
+    def __init__(self, request):
+
+        self.session = request.session
+        cart = self.session.get('skey')
+        if 'skey' not in request.session:
+            cart = self.session['skey'] = {'number': 33333}
+        self.cart = cart
